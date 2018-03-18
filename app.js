1c0afe4828775741082e446f5bd5c30f9c48689b
[kashinkou] <[kashinkou@gmail.com]>
2018-03-18 13:32:55 -0700

    event

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ $(function(){
 		$('img').attr('src','http://media.blizzard.com/wow/media/wallpapers/bosses/lich-king/lich-king-800x600.jpg');
 	});
 	$('#input').on('change', function() {
-    var val;
-    val = $('#input').val();
-    $('.content').text(val);
+	var val;
+	val = $('#input').val();
+	$('.content').text(val);
 	});
 })
 
