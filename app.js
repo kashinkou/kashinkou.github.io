b91bea501a3fa80e0131cd7d4673847c318f9f2f
[kashinkou] <[kashinkou@gmail.com]>
2018-03-18 13:38:18 -0700

    event

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 
 */
 $(function(){
+	$('img').attr('src','http://media.blizzard.com/wow/media/wallpapers/bosses/lich-king/lich-king-800x600.jpg');
 	$('#button').on('click', function(){
 		$('body').css('background-color', 'black');
-		$('img').attr('src','http://media.blizzard.com/wow/media/wallpapers/bosses/lich-king/lich-king-800x600.jpg');
+		//$('img').attr('src','http://media.blizzard.com/wow/media/wallpapers/bosses/lich-king/lich-king-800x600.jpg');
 	});
 	$('#input').on('change', function() {
 	var val;
