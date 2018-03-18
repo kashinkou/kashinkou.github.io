041c749e909059ef2f28172519665d4de04b3984
[kashinkou] <[kashinkou@gmail.com]>
2018-03-18 13:26:15 -0700

    add event

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+
+For this quiz, can you use this script, which is linked in the <head> of index.html,
+to change the boring placeholder image to a picture of a cute animal?
+
+Remember, you'll need to pass a function into the jQuery object to run
+when the document is ready.
+
+Unfortunately, placepuppy is no longer available. Here's a link to a random
+animal image on lorempixel.com:
+http://lorempixel.com/350/150/animals/
+
+Good luck!
+
+*/
+$(function(){
+	$('#button').on('click', function(){
+		$('body').css('background-color', 'black');
+		$('img').attr('src','http://media.blizzard.com/wow/media/wallpapers/bosses/lich-king/lich-king-800x600.jpg');
+	});
+	$('#input').on('change', function() {
+    var val;
+    val = $('#input').val();
+    $('.content').text(val);
+	});
+})
+
